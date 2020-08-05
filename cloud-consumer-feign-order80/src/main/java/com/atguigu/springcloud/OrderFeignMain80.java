2fb32bbf7298b82d89e2d8142f962ab00606b008
huyang <1981115225@qq.com>
2020-08-05 16:43:16 +0800

    Feign 远程调用消费者

--- cloud-consumer-feign-order80/src/main/java/com/atguigu/springcloud/OrderFeignMain80.java
+++ cloud-consumer-feign-order80/src/main/java/com/atguigu/springcloud/OrderFeignMain80.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
+ * Feign 远程调用消费者
  * @author huyang
  * @date 2020/5/16 19:30
  */
