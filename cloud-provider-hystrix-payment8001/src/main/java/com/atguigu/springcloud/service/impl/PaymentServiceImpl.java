16c2dc46e038be5e77bffe89e47c4a40798aed47
huyang <1981115225@qq.com>
2020-08-05 10:37:38 +0800

    完善 服务降级和熔断

--- cloud-provider-hystrix-payment8001/src/main/java/com/atguigu/springcloud/service/impl/PaymentServiceImpl.java
+++ cloud-provider-hystrix-payment8001/src/main/java/com/atguigu/springcloud/service/impl/PaymentServiceImpl.java
@@ -30,7 +30,8 @@ public class PaymentServiceImpl implements PaymentService {
             // 假设3秒钟是正常业务逻辑，如果超过3秒就会降级处理
             @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "3000")})
     public String paymentInfo_timeout(Integer id) {
-        int timeNumber = 2;
+        int timeNumber = id;
+        // 出异常还是超过设定的时间峰值，都会进入服务降级所指定的方法
 //        int age = 10/0;
         try {
             TimeUnit.SECONDS.sleep(timeNumber);
@@ -48,12 +49,12 @@ public class PaymentServiceImpl implements PaymentService {
     /**
      * 下面是服务熔断
      */
-@HystrixCommand(fallbackMethod = "paymentCircuitBreaker_fallback",commandProperties = {
-        @HystrixProperty(name = "circuitBreaker.enabled",value = "true"),// 是否开启断路器
-        @HystrixProperty(name = "circuitBreaker.requestVolumeThreshold",value = "10"),// 请求次数
-        @HystrixProperty(name = "circuitBreaker.sleepWindowInMilliseconds",value = "10000"),// 时间范围
-        @HystrixProperty(name = "circuitBreaker.errorThresholdPercentage",value = "60") // 失败率达到多少后跳闸
-})
+    @HystrixCommand(fallbackMethod = "paymentCircuitBreaker_fallback", commandProperties = {
+            @HystrixProperty(name = "circuitBreaker.enabled", value = "true"),// 是否开启断路器
+            @HystrixProperty(name = "circuitBreaker.requestVolumeThreshold", value = "10"),// 请求次数
+            @HystrixProperty(name = "circuitBreaker.sleepWindowInMilliseconds", value = "10000"),// 时间范围
+            @HystrixProperty(name = "circuitBreaker.errorThresholdPercentage", value = "60") // 失败率达到多少后跳闸
+    })
     @Override
     public String paymentCircuitBreaker(@PathVariable("id") Integer id) {
         if (id < 0) {
@@ -65,6 +66,6 @@ public class PaymentServiceImpl implements PaymentService {
     }
 
     public String paymentCircuitBreaker_fallback(@PathVariable("id") Integer id) {
-        return "id 不能负数，请稍候再试,(┬＿┬)/~~     id: " + id;
+        return "进入熔断了,id 不能负数，请稍候再试,(┬＿┬)/~~     id: " + id;
     }
 }
