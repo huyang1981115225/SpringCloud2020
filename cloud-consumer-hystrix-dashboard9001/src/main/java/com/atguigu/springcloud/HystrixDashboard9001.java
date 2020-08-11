e9f7b855fadf823bcb0187284e29b0454255c81b
huyang <1981115225@qq.com>
2020-08-11 10:56:07 +0800

    HystrixDashBoard：Hystrix进行实时监控的工具   熔断监控面板  2020/08/11

--- cloud-consumer-hystrix-dashboard9001/src/main/java/com/atguigu/springcloud/HystrixDashboard9001.java
+++ cloud-consumer-hystrix-dashboard9001/src/main/java/com/atguigu/springcloud/HystrixDashboard9001.java
@@ -1,13 +1,20 @@
 package com.atguigu.springcloud;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 /**
  * @author huyang
  * @date 2020/5/17 11:13
  */
+@EnableHystrixDashboard
+@EnableDiscoveryClient
 @SpringBootApplication
-
 public class HystrixDashboard9001 {
+    public static void main(String[] args) {
+        SpringApplication.run(HystrixDashboard9001.class, args);
+    }
 }
