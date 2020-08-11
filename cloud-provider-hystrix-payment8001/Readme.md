948b9dcdf59b38da8ee293c879021ca7d6a4b4f2
huyang <1981115225@qq.com>
2020-08-11 10:54:55 +0800

    HystrixDashBoard：Hystrix进行实时监控的工具   熔断监控面板  2020/08/11

---
+++ cloud-provider-hystrix-payment8001/Readme.md
@@ -0,0 +1,10 @@
+# HystrixDashboard 怎么看视图界面
+
+## 一、7色 ：Success | Short-Circuited | Bad Request | Timeout | Rejected | Failure | Error  
+
+## 二、1圈：实心圆：
+
+    1. 颜色的变化：代表了实例的健康程度，它的健康程度从 绿色 < 黄色 < 橙色 < 红色；
+    2. 大小的变化：代表了实例请求的流量，流量越大，该实心圆越大；
+
+## 三、1线：两分钟内流量的相对变化，通过它可以来观察流量的上升和下降趋势
--- cloud-provider-hystrix-payment8001/src/main/java/com/atguigu/springcloud/PaymentHystrixMain8001.java
+++ cloud-provider-hystrix-payment8001/src/main/java/com/atguigu/springcloud/PaymentHystrixMain8001.java
@@ -1,11 +1,19 @@
 package com.atguigu.springcloud;
 
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
 
 /**
+ * 9001 监控 8001:
+ * 1、浏览器 访问 ： http://localhost:9001/hystrix
+ * 2、http://localhost:8001/hystrix.stream
+ * 3、Dalay：2000
+ * 4、Title：服务名
  * SpringCloud  带熔断和降级的服务提供方  Hystrix
  * @author huyang
  * @date 2020/5/16 20:35
@@ -17,5 +25,19 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 public class PaymentHystrixMain8001 {
     public static void main(String[] args) {
         SpringApplication.run(PaymentHystrixMain8001.class, args);
+    }
+
+    /**
+     * 此配置是为了服务监控而配置，与服务容错本身无关，SpringCloud升级后的坑
+     * @return
+     */
+    @Bean
+    public ServletRegistrationBean getServlet() {
+        HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
+        ServletRegistrationBean registrationBean = new ServletRegistrationBean(streamServlet);
+        registrationBean.setLoadOnStartup(1);
+        registrationBean.addUrlMappings("/hystrix.stream");
+        registrationBean.setName("HystrixMetricsStreamServlet");
+        return registrationBean;
     }
 }
